lzw29107/qemu

commit c9349965ce4a1548a0b19dc3cf859a461d525f6b

tcg: Optimize INDEX_op_mul[us]2 for 0 and 1
Zero operands produce a zero high and low product. One operands produce
a copy of the other operand and a zero or sign extension in the high
half.

Fold those cases during TCG optimization so wide-multiply idioms used by
target translators can collapse before code generation.

Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
Message-ID: <20260520125139.13352-3-philmd@linaro.org>

tcg/optimize.c
@@ -2212,6 +2212,34 @@ static bool fold_multiply2(OptContext *ctx, TCGOp *op)
             tcg_opt_gen_movi(ctx, op2, rh, h);
             return true;
         }
+
+        if (b == 0) {
+            op2 = opt_insert_before(ctx, op, 0, 2);
+            tcg_opt_gen_movi(ctx, op2, rl, 0);
+            tcg_opt_gen_movi(ctx, op, rh, 0);
+            return true;
+        }
+        if (b == 1) {
+            op2 = opt_insert_before(ctx, op, 0, 2);
+            tcg_opt_gen_mov(ctx, op2, rl, op->args[2]);
+
+            switch (op->opc) {
+            case INDEX_op_mulu2:
+                tcg_opt_gen_movi(ctx, op, rh, 0);
+                break;
+            case INDEX_op_muls2:
+                op->opc = INDEX_op_sar;
+                op->args[0] = rh;
+                op->args[1] = rl;
+                op->args[2] =
+                    arg_new_constant(ctx, tcg_type_size(ctx->type) * 8 - 1);
+                break;
+            default:
+                g_assert_not_reached();
+            }
+
+            return true;
+        }
     }
     return finish_folding(ctx, op);
 }

commit 5a2871041a440c4b387a7b057eadd5f3d040265c

tcg/optimize: Update masks when folding muls2 by one
When folding INDEX_op_muls2 with a constant multiplier of one,
the operation is rewritten as an arithmetic shift right to produce
the high half of the result.

However, directly changing the opcode bypasses the normal mask
propagation performed for shift operations, leaving stale TempOptInfo
state for the high result. This can lead to incorrect subsequent
optimizations.

Use fold_masks_s() to update the output masks based on the source
sign mask after expanding the operation.

Fixes: c9349965ce ("tcg: Optimize INDEX_op_mul[us]2 for 0 and 1")

tcg/optimize.c
@@ -2222,6 +2222,7 @@ static bool fold_multiply2(OptContext *ctx, TCGOp *op)
         TCGArg rl = op->args[0];
         TCGArg rh = op->args[1];
         TCGOp *op2;
+        TempOptInfo *ti;
 
         if (arg_is_const(op->args[2])) {
             uint64_t a = arg_const_val(op->args[2]);
@@ -2277,7 +2278,9 @@ static bool fold_multiply2(OptContext *ctx, TCGOp *op)
                 op->args[1] = rl;
                 op->args[2] =
                     arg_new_constant(ctx, tcg_type_size(ctx->type) * 8 - 1);
-                break;
+
+                ti = arg_info(rl);
+                return fold_masks_s(ctx, op, ti->s_mask);
             default:
                 g_assert_not_reached();
             }